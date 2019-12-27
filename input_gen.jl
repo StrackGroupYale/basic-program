a595386769a8ae301b0ad471402d483671c63e11
Joshua M. V. Purtell <josh.purtell@yale.edu>
2019-12-27 18:54:11 -0500

    Undebugged version, no shock support yet, pending

--- input_gen.jl
+++ input_gen.jl
@@ -1,26 +1,16 @@
-######READREADREAD
-#Josh: Ephraim, could you work on the input_2_tuple function? Ideally the user would be able to just type in the number of means, 
-#each of the means, and either the scale factors or N/A (which would create the default factor of 1) which the function would
-#use to create the tuple that gets fed into the distribution generator
-
-#Inputs: num, means_vec
-#Output file: num_types,num_objects,Util_vec,Dist_arr,Cap_Constraint_vec
+##Undebugged
 
 using Distributions
 using JuMP
-
-#Accept User Input
-function input_2_tuple()
-
-  return meanscale_vec
-end
+using CSV
+using DataFrames
 
 
 #Shock Regime
 
 function shock_gen_incr(shock_num,shock_base,shock_inc)
 	shock = Vector{T}(float, n)
-	for i:shock_num
+	for i in 1:shock_num
 		shock[i] = shock_base + shock_inc*(i-1)
 	end
 	return shock
@@ -32,12 +22,57 @@ function shocks(Util,shock)
 	return s_Util
 end
 
-#Logistic Distribution Generator
 
-function logistic(meanscale_vec)
-    d[i]= Logistic(meanscale_vec[i],meanscale_vec[num+i]) ##create logistic function: mean, scale
+#Logistic Distribution Generator
+function log_dist_gen(frm)
+	#find number of rows
+	m = size(frm,1)
+	#find number of columns
+	n = size(frm,2)
+	#logstic fxns
+	d = Array{T}(undef, m, n)
+	for (i in 1:n, isodd(i)==true) #only index over means
+		for i in 1:m
+			d[i]= Logistic(frm[i,n],frm[i,n+1]) ##create logistic function: mean, scale
+		end
+	end
+	return d
 end
-    
+		
+	
+#input file gen
+function data_gen(file_util,file_dist,file_cap)
+	print("Do you wish to input data directly (or have you invoked using a CSV file)? Y or (N)")
+	if readline() == Y
+		print("Please enter the following UTILITY info in CSV format: col1:means,col2:scalefactor")
+		data = CSV.File(readline())
+		frm_util = CSV.read(data)
+		print("Please enter the following DISTANCE info in CSV format: colx:means_distfromx,colx+1:scalefactor")
+		data = CSV.File(readline())
+		frm_dist = CSV.read(data)
+		print("Please enter the following CAPACITY info in CSV format: col1:means,col2:scalefactor")
+		data = CSV.File(readline())
+		frm_cap = CSV.read(data)
+	end
+	if readline() == N
+		#covert to dataframe
+		frm_util = CSV.read(CSV.File(file_util))
+		frm_dist = CSV.read(CSV.File(file_dist))
+		frm_cap = CSV.read(CSV.File(file_cap))
+	end
+	else
+	#Make Log Dists
+	util_dist = log_dist_gen(frm_util)
+	dist_dist = log_dist_gen(frm_dist)
+	cap_dist = log_dist_gen(frm_cap)
+	
+	num_types = nrow(frm_util)
+	num_objects = nrow(frm_cap)
+	
+	#Output file: num_types,num_objects,Util_df,Dist_df,Cap_Constraint_df
+	return (num_types,num_objects,util_dist,dist_dist,cap_dist)
+end
+
 
 
 
