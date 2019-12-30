14affa28e8cbc4652113d718e3f34836d46e5185
Joshua M. V. Purtell <josh.purtell@yale.edu>
2019-12-30 16:46:45 -0500

    Updated, workable

--- solver_basic/general_fxn.jl
+++ solver_basic/general_fxn.jl
@@ -14,23 +14,57 @@
 using JuMP
 using Cbc
 
+using MathOptInterface
+const MOI = MathOptInterface
+
 ##Mechanism Function
-##Function Inputs: # types, # objects, utility vector, Distribution, quantity constraints
-function mech_basic(num_types,num_objects,Util_vec,Dist_arr,Cap_Constraint_vec)
+##Function Inputs: # types # objects, type vectors, distribution, quantity constraints
+
+#Vectorization
+
+function df2vec(df,dimlen)
+	vec = Array{Float64}(undef, dimlen)
+	for i in 1:dimlen
+		vec[i] = df[i,1]
+	end
+	return vec
+end
+
+function df2arr(df,dim1len,dim2len)
+	arr = Array{Float64}(undef, dim1len, dim2len)
+	for i in 1:dim1len
+		for j in 1:dim2len
+			arr[i,j] = df[i,j]
+		end
+	end
+	return arr
+end
+
+function mech_basic_cbc(num_types,num_objects,type_vec2,type_probs,cap_vec,Dist_arr)
+	#old (num_types,num_objects,Util_vec,Dist_arr,cap_vec)
+
+	#Util_vec = df2vec(util_dist,num_types)
+	#Distribution_arr = df2arr(frm_dist,num_types,num_objects)
+	#cap_vec = df2vec(frm_cap,num_objects)
+
+	#generate type_arr
+	type_arr = Array{Float64}(undef, num_types,num_objects)
+	for i in 1:num_types
+		for j in 1:num_objects
+			type_arr[i,j] = type_vec2[i][j]
+		end
+	end
 
 	T = num_types #rows, represents length of Theta
 	A = num_objects #columns, represents length of A
 
-	#Reshape utility dataset
-	Util_arr = reshape(Util_vec, (T,A) )
-
 	m = Model(with_optimizer(Cbc.Optimizer, logLevel=1))
-	
+
 	#Alloc variable
 	@variable(m, X[1:T,1:A])
 
 	#capacity constraint
-	@constraint(m, Ccon, transpose(Dist_arr)*X .<= Cap_Constraint_vec)
+	@constraint(m, Ccon, Dist_arr*X .<= cap_vec) #@constraint(m, Ccon, transpose(Dist_arr)*X .<= cap_vec)
 
 
 	#feasibility constraint
@@ -40,13 +74,22 @@ function mech_basic(num_types,num_objects,Util_vec,Dist_arr,Cap_Constraint_vec)
 	@constraint(m, Fcon, ones_T*X .<= 1)
 
 	#IC constraint
-	@constraint(m, con[i=1:T,j=1:T], sum( Util_arr[i,k]*X[i,k] for k in 1:A)-sum( Util_arr[j,k]*X[j,k] for k in 1:A)>= 0)
+	@constraint(m, con[i=1:T,j=1:T], sum( type_arr[i,k]*X[i,k] for k in 1:A)-sum( type_arr[j,k]*X[j,k] for k in 1:A)>= 0)
 
 	#objective
-	@objective(m, Max, sum( Dist_arr[i]*(transpose(X[i,:])*Util_arr[i,:]) for i in 1:T))
+	@objective(m, Max, sum( Dist_arr[i]*(transpose(X[i,:])*type_arr[i,:]) for i in 1:T))
 
 	#print(m)
 	status = optimize!(m)
-	
-  return ("info, time: ", JuMP.objective_value(m), @time JuMP.objective_value(m))
+
+	#j = MOI.get(Cbc.Optimizer, VariablePrimal(), X)
+
+  return (status,j)
+end
+
+#fix
+function compos_cbc(utility_means,shocks,shock_distribution,capacities)
+	in = data_gen(utility_means,shocks,shock_distribution,capacities)
+	out = mech_basic_cbc(in[1],in[2],in[3],in[4],in[5],in[6])
+	return out
 end
