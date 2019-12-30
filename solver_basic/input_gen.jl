3f326a2baf4bd1a2246fa0e50d8cd76c0418e5c3
Joshua M. V. Purtell <josh.purtell@yale.edu>
2019-12-30 15:32:00 -0500

    Updated, Debugged

--- solver_basic/input_gen.jl
+++ solver_basic/input_gen.jl
@@ -1,110 +1,98 @@
 ##Debugged
 
+###input utility value of schools, potential shocks, distribution of shocks (agent types), capacity, distances,
+
+###output mech_basic(num_types,num_objects,type_arr,distribution_arr,capacity_vec)
+
 using Distributions
 using JuMP
 using CSV
 using DataFrames
 
-
-#Logistic Distribution Generator
-function log_dist_gen(frm)
-	#find number of rows
-	m = size(frm,1)
-	#logstic fxns
-	d = Array{Float64}(undef, m)
-	for i = 1:m
-		mean = frm[i,1]
-		scalar = frm[i,2]
-		d[i]= rand(Logistic(mean,scalar)) ##create logistic function: mean, scale; then, pick a random sample
-	end
-	return d
-end
-
-function extract_mean(frm)
-	#find number of rows
-	m = size(frm,1)
-	#logstic fxns
-	d = Array{Float64}(undef, m)
-	for i = 1:m
-		mean = frm[i,1]
-		scalar = frm[i,2]
-		d[i]= mean ##create logistic function: mean, scale; then, pick a random sample
-	end
-	return d
-end
-
-function vec2df(vec)
-	df = DataFrame()
-	df.util_mean = vec
-	return df
-end
-
-
-#Generate shock vector, given the length,base shock, and increment
-function shock_gen_incr(shock_len,shock_base,shock_inc)
-	shock = Vector{Float64}(undef, shock_len)
-	for i in 1:shock_len
-		shock[i] = shock_base + shock_inc*(i-1)
-	end
-	return shock
-end
-
-
-
 ###input file gen
 
-function data_gen(file_util,file_dist,file_cap,logi_bool,incr_bool,incr_num,incr_base,incr_incr)
+function quantile_prob_finder(distribution, quant)
+	d = 0.0
+	if (distribution == "logistic")
+		d = pdf(Logistic(),quantile(Logistic(),quant*(0.01)))
+	elseif (distribution == "uniform")
+		d = pdf(Logistic(),quantile(Uniform(),quant*(0.01)))
+	else
+		printline("Error: distribution type not supported")
+	end
+	return d
+end
+
+
+function data_gen(utility_means,shocks,shock_distribution,capacities)
+
+#outputs: (num_types,num_objects,type_vec2,distribution,capacity_vec)
+
+#utility, shock, shock distribution, variance, capacity,
 #takes (CSV,CSV,CSV,2-vector,3-vector)
 
-	#covert to dataframe
-	frm_util = DataFrame(CSV.File(file_util))
-	frm_dist = DataFrame(CSV.File(file_dist))
-	frm_cap = DataFrame(CSV.File(file_cap))
-	if (logi_bool+incr_bool>1)
-		#done
-		if logi_bool ==1
-			util_dist = log_dist_gen(frm_util)
-			shck = shock_gen_incr(incr_num,incr_base,incr_incr)
-			util = Vector{Any}(undef, incr_num)
-			for i in 1:incr_num
-				util[i] = vec2df(broadcast(+,shck[i],util_dist))
-			end
-			util_dist = util
-		end
-		#done
-		if logi_bool == 2
-			shck = shock_gen_incr(incr_num,incr_base,incr_incr)
-			util_dist = Vector{Any}(undef, incr_num)
-			for i in 1:incr_num
-				for j in 1:nrow(frm_util)
-					frm_util[j,1] = frm_util[j,1] + shck[i]
-				end
-				util_dist[i] = vec2df(log_dist_gen(frm_util))
-			end
-		end
+	#covert information to dataframe
+	frm_util = DataFrame(CSV.File(utility_means))
+	frm_shocks = DataFrame(CSV.File(shocks))
+	frm_cap = DataFrame(CSV.File(capacities))
+
+	num_types = nrow(frm_util)*ncol(frm_shocks) #assuming shocks given with objects in row, each vector as a column
+	num_objects = nrow(frm_util)
+
+	#convert dataframe to matrix
+	util_arr1 = Vector{Float64}(undef, nrow(frm_util))
+	for i in 1:nrow(frm_util)
+		util_arr1[i] = frm_util.mean[i]
 	end
 
-	if (logi_bool+incr_bool<=1)
-		#done
-		if logi_bool == 1
-			util_dist = vec2df(log_dist_gen(frm_util))
+	shocks_arr2 = Vector{Any}(undef, ncol(frm_shocks))
+	for i in 1:ncol(frm_shocks)
+		shockvec = Vector{Float64}(undef, nrow(frm_shocks))
+		for j in 1:nrow(frm_shocks)
+			shockvec[j]= frm_shocks[:,i][j]
 		end
-		#done
-		if incr_bool == 1
-			shck = shock_gen_incr(incr_num,incr_base,incr_incr)
-			util_dist = Vector{Any}(undef, incr_num)
-			for i in 1:incr_num
-				for j in 1:nrow(frm_util)
-					frm_util[j,1] = frm_util[j,1] + shck[i]
-				end
-				util_dist[i] = vec2df(extract_mean(frm_util))
-			end
-		end
+		shocks_arr2[i] = shockvec
 	end
 
-	num_types = nrow(frm_util)
-	num_objects = nrow(frm_cap)
+	cap_vec = Vector{Float64}(undef, nrow(frm_cap))
+	for i in 1:nrow(frm_cap)
+		cap_vec[i] = frm_cap.cap[i]
+	end
 
-	#Output file: num_types,num_objects,Util_df,Dist_df,Cap_Constraint_df
-	return (num_types,num_objects,util_dist,frm_dist,frm_cap)
+	##assume independence across objects
+
+	##find object shock probabilities, for discrete whole-number quantiles
+	quantile_probs = Vector{Any}(undef, 100)
+	for i in 1:100
+		quantile_probs[i] = quantile_prob_finder(shock_distribution,i)
+	end
+	total = sum(quantile_probs)
+
+	#normalize
+	for i in 1:100
+		quantile_probs[i] = quantile_probs[i]/total
+	end
+
+	##generate distribution for types/shock vectors
+	type_probs = Vector{Float64}(undef, ncol(frm_shocks))
+	for i in 1:ncol(frm_shocks)
+		prob = 1
+		for j in 1:nrow(frm_shocks)
+			prob = prob*quantile_probs[Int(shocks_arr2[i][j]*100)]
+		end
+		type_probs[i] = prob
+	end
+	total2 = sum(type_probs)
+
+	##re-normalize, given actual
+	for i in 1:ncol(frm_shocks)
+		type_probs[i] = type_probs[i]/total2
+	end
+
+	##generate types
+	type_vec2 = Vector{Any}(undef, ncol(frm_shocks))
+	for i in 1:ncol(frm_shocks)
+		type_vec2[i] = broadcast(+,util_arr1,shocks_arr2[i])
+	end
+	return (num_types,num_objects,type_vec2,type_probs,cap_vec)
 end
