3fcee9dc299b430de76d6aec76c3a351f7c9705d
Joshua M. V. Purtell <josh.purtell@yale.edu>
2019-12-30 15:40:18 -0500

    Updated with instructions

--- solver_basic/MAKEFILE.jl
+++ solver_basic/MAKEFILE.jl
@@ -1,1 +1,9 @@
 #MAKEFILE.jl
+#In order to run data_gen.jl:
+  #add the following pkgs: Distributions,JuMP,CSV,DataFrames
+  #Make sure csv files are downloaded (check folder), and identify correct paths for each
+  #build data_gen.jl with include("/path/data_gen.jl")
+  #invoke data_gen("/path/util_data.csv","/path/dist_data.csv","logistic","/path/cap_data.csv")
+  #or
+  #invoke data_gen("/path/util_data.csv","/path/dist_data.csv","uniform","/path/cap_data.csv")
+  
