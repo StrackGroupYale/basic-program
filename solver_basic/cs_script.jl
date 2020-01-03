ffb2239ac543c3bd829c8527b588045f5b917756
Joshua M. V. Purtell <josh.purtell@yale.edu>
2020-01-03 14:43:51 -0500

    updated for terminal use

--- solver_basic/cs_script.jl
+++ solver_basic/cs_script.jl
@@ -1,12 +1,11 @@
-push!(LOAD_PATH, "/Users/joshuapurtell/documents/Github/basic-program/solver_basic")
+push!(LOAD_PATH, "basic-program/solver_basic")
 println(LOAD_PATH)
 
 using problem_generator
 using problem_solver
 using CSV_gen
 
-problem_cs("~./basic_program/solver_basic/2x3_example/util_data.csv",
-           "~./basic_program/solver_basic/2x3_example/shock_data.csv",
+problem_solver.problem_cs("basic-program/solver_basic/2x3_example/util_data.csv",
+           "basic-program/solver_basic/2x3_example/shock_data.csv",
            "logistic",
-           "~./basic_program/solver_basic/2x3_example/cap_data.csv")
-           
+           "basic-program/solver_basic/2x3_example/cap_data.csv")
