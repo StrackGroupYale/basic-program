5be4433c7e343e1cc027618b9f07cf6ae215f38d
Joshua M. V. Purtell <josh.purtell@yale.edu>
2020-01-03 13:18:13 -0500

    Create cs_script.jl

---
+++ solver_basic/cs_script.jl
@@ -0,0 +1,6 @@
+
+problem_cs("~./basic_program/solver_basic/2x3_example/util_data.csv",
+           "~./basic_program/solver_basic/2x3_example/shock_data.csv",
+           "logistic",
+           "~./basic_program/solver_basic/2x3_example/cap_data.csv")
+           
