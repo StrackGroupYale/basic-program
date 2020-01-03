2cf07925d1d54c73c8fff072cd0180027799ceaf
Joshua M. V. Purtell <josh.purtell@yale.edu>
2020-01-03 12:30:56 -0500

    Update pkgs2add.jl

--- solver_basic/pkgs2add.jl
+++ solver_basic/pkgs2add.jl
@@ -14,7 +14,3 @@ Pkg.build("DataFrames")
 Pkg.build("Distributions")
 Pkg.build("MathOptInterface")
 Pkg.build("JuMP")
-
-include("/Users/joshuapurtell/Desktop/Strack_Project/problem_generator.jl")
-include("/Users/joshuapurtell/Desktop/Strack_Project/problem_solver.jl")
-include("/Users/joshuapurtell/Desktop/Strack_Project/CSV_gen.jl")
