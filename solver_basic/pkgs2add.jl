6d54fba4b2679480e127bab51387adf7ef6ee723
Joshua M. V. Purtell <josh.purtell@yale.edu>
2020-01-05 23:25:55 -0500

    added JLD

--- solver_basic/pkgs2add.jl
+++ solver_basic/pkgs2add.jl
@@ -6,6 +6,7 @@ Pkg.add("DataFrames")
 Pkg.add("Distributions")
 Pkg.add("MathOptInterface")
 Pkg.add("JuMP")
+Pkg.add("JLD")
 
 
 Pkg.build("CSV")
@@ -14,3 +15,4 @@ Pkg.build("DataFrames")
 Pkg.build("Distributions")
 Pkg.build("MathOptInterface")
 Pkg.build("JuMP")
+Pkg.build("JLD")
