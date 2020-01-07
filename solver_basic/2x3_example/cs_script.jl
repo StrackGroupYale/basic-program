55e5e82ceb5c92b4e77dac7bc79cccea3e03cc97
Joshua M. V. Purtell <josh.purtell@yale.edu>
2020-01-07 17:25:39 -0500

    Update cs_script.jl

--- solver_basic/2x3_example/cs_script.jl
+++ solver_basic/2x3_example/cs_script.jl
@@ -5,6 +5,7 @@ using problem_generator
 using problem_solver
 using exec_and_time
 
+##Load results to verify concurrance
 exec_and_time.problem_glpk("basic-program/solver_basic/2x3_example/util_data.csv",
            "basic-program/solver_basic/2x3_example/shock_data.csv",
            "logistic",
@@ -14,3 +15,18 @@ exec_and_time.problem_cbc("basic-program/solver_basic/2x3_example/util_data.csv"
            "basic-program/solver_basic/2x3_example/shock_data.csv",
            "logistic",
            "basic-program/solver_basic/2x3_example/cap_data.csv")
+
+##Yield time results
+x = @elapsed exec_and_time.problem_glpk("basic-program/solver_basic/2x3_example/util_data.csv",
+           "basic-program/solver_basic/2x3_example/shock_data.csv",
+           "logistic",
+           "basic-program/solver_basic/2x3_example/cap_data.csv")
+y = @elapsed exec_and_time.problem_cbc("basic-program/solver_basic/2x3_example/util_data.csv",
+           "basic-program/solver_basic/2x3_example/shock_data.csv",
+           "logistic",
+           "basic-program/solver_basic/2x3_example/cap_data.csv")
+println("glpk: ",x," cbc: ",y)
+
+####For other solvers, simply write a corresponding "problem" function in "exec_and_time.jl",
+####Then add correpsonding "mech_basic" functions in problem_solver.jl
+####Then write the corresponding timing functions in this script
