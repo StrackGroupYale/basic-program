6a1feeb2e0558757c8b4e5ecc314ef9888768f22
Joshua M. V. Purtell <josh.purtell@yale.edu>
2020-01-07 17:18:52 -0500

    Update cs_script.jl

--- solver_basic/2x3_example/cs_script.jl
+++ solver_basic/2x3_example/cs_script.jl
@@ -5,8 +5,12 @@ using problem_generator
 using problem_solver
 using exec_and_time
 
+exec_and_time.problem_glpk("basic-program/solver_basic/2x3_example/util_data.csv",
+           "basic-program/solver_basic/2x3_example/shock_data.csv",
+           "logistic",
+           "basic-program/solver_basic/2x3_example/cap_data.csv")
 
-exec_and_time.problem_cs("basic-program/solver_basic/2x3_example/util_data.csv",
+exec_and_time.problem_cbc("basic-program/solver_basic/2x3_example/util_data.csv",
            "basic-program/solver_basic/2x3_example/shock_data.csv",
            "logistic",
            "basic-program/solver_basic/2x3_example/cap_data.csv")
