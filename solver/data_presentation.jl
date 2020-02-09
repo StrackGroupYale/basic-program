c9fdd7528a6290b5285bbf4cf5e45bc321667959
Josh <josh.purtell@yale.edu>
2020-02-09 00:31:27 -0500

    Update data_presentation.jl

--- solver/data_presentation.jl
+++ solver/data_presentation.jl
@@ -1,8 +1,9 @@
 using CSV
 
 function plot(a_file,t_file)
-    a_df = CSV.read(a_file,header=0)
     t_df = CSV.read(t_file,header=0)
+    a_df = CSV.read(a_file,header=0)
+    print(a_df)
 end
 
 plot("solver/assignment_data/a_data@2,9.csv","solver/assignment_data/t_data@2,9.csv")
