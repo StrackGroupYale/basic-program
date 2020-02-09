9df570e7c70ec43f4e09f9d4d3f560cf0271c388
Josh <josh.purtell@yale.edu>
2020-02-08 20:37:03 -0500

    update

--- solver/data_presentation.py
+++ solver/data_presentation.py
@@ -1,10 +1,11 @@
 #present data
-import pandas as pd
+#import pandas as pd
+import numpy as np
 
-#def readable_table(distributions,types):
-    #dist_df = pd.read_csv(distributions)
-    #type_df = pd.read_csv(types)
-    #return 2
+def readable_table(distributions,types):
+    dist_df = pd.read_csv(distributions)
+    type_df = pd.read_csv(types)
+    return 2
 if __name__ == "__main__":
     2+2
     #print('yeah')
