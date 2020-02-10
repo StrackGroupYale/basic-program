a3b20502e8b6c7415e8cb162f287b2626400aed9
Josh <josh.purtell@yale.edu>
2020-02-09 20:55:21 -0500

    Update to show shocked utilities

--- solver/data_presentation.py
+++ solver/data_presentation.py
@@ -4,22 +4,30 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def con_df(distributions,types):
+def con_df(distributions,types,utilities):
     dist_df = pd.read_csv(distributions,header=None)
     type_df = pd.read_csv(types,header=None)
+    ut_df = pd.read_csv(utilities,header=None)
 
     #create column names
     type_names = []
     for i in range(0,len(type_df.columns)):
         type_names.append(f'Shock for School {i+1}')
     type_df.columns = type_names
+
+    ut_names = []
+    for i in range(0, len(ut_df.columns)):
+        ut_names.append(f'Utility from School {i + 1}')
+    ut_df.columns = ut_names
+
     dist_names = []
     for i in range(0, len(dist_df.columns)):
         dist_names.append(f'Allocation for School {i + 1}')
     dist_df.columns = dist_names
-    df = pd.concat([dist_df,type_df], axis=1)
+    df = pd.concat([dist_df,type_df,ut_df], axis=1)
 
     return df
+
 def write_table(df):
     fig = plt.figure(facecolor='w', edgecolor='k')
     sns.heatmap(df,annot=True, cmap='viridis',square=False)
@@ -28,5 +36,5 @@ def write_table(df):
 
 
 if __name__ == "__main__":
-    df = con_df('assignment_data/a_data@2,9.csv','assignment_data/rt_data@2,9.csv')
+    df = con_df('assignment_data/a_data@2,9.csv','assignment_data/rt_data@2,9.csv','assignment_data/t_data@2,9.csv')
     write_table(df)
