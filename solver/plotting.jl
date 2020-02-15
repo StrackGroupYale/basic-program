71c3ed80a95128ee3bd3a7598c7d7f83cf8b8738
Josh <josh.purtell@yale.edu>
2020-02-15 12:15:10 -0500

    trivial

--- solver/plotting.jl
+++ solver/plotting.jl
@@ -1,14 +1,15 @@
 using Plots
 using CSV
-
-function plot_performance(shocks,utilities)
-    df = CSV.read("solver/test_data/time@$shocks,$utilities.csv")
-    x = df[!,:types]
-    y = df[!,:time]
-    plot(x,y,title="Time to Solve Over Number of Types")
-    xlabel!("Number of Types")
-    ylabel!("Seconds")
-    savefig("solver/test_data/time_vs_complexity@$shocks,$utilities.png")
-end
+module plotting
+    export plot_performance
+    function plot_performance(shocks,utilities)
+        df = CSV.read("solver/test_data/time@$shocks,$utilities.csv")
+        x = df[!,:types]
+        y = df[!,:time]
+        plot(x,y,title="Time to Solve Over Number of Types")
+        xlabel!("Number of Types")
+        ylabel!("Seconds")
+        savefig("solver/test_data/time_vs_complexity@$shocks,$utilities.png")
+    end
 
 plot_performance(3,13)
